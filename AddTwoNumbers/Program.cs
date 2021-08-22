f1e7ee709fb5ca21d78eecdb1b13236367521569
Bao D Pham <bdpham00@gmail.com>
2021-08-21 23:09:09 -0700

    Linked List Commit

---
+++ AddTwoNumbers/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AddTwoNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedListSolution solution = new LinkedListSolution();
+            var ans = solution.AddTwoNumbers(new ListNode(2, new ListNode(4, new ListNode(3))), new ListNode(5, new ListNode(6, new ListNode(4))));
+            Console.WriteLine(ans);
+            Console.Read();
+        }
+    }
+}
